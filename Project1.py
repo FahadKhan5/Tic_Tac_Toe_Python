1afcca302c5164af4bcf384335f06b2d010577e8
Fahad Faheem <68124886+FahadKhan5@users.noreply.github.com>
2023-09-17 11:02:05 +0500

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,111 @@
+#print("hello")
+#print("There is an old saying \"Do good, have good\". What a wonderful quotation!")
+#print("Hey", 1, 2, sep="~", end="\n")
+#print("Python")
+
+"""a = complex(2,3)
+b = True
+c = "Python"
+d = None
+a1 = 9
+print(a+a1)
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+print("The type of d is", type(d))
+print("The type of d is", type(d))
+print("The type of d is", type(d))
+print("The type of d is", type(d))
+print("The type of d is", type(d)) #alt + shift + down arrow the line will replicate
+
+#Calculator 
+print("This is a Calculator")
+
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+
+add = a +b
+print("The addition of ", a, "and" , b, "is", add)
+
+sub = a-b
+print("The subtraction of ", a, "and" , b, "is", sub)
+
+mult = a*b
+print("The mlutiplication of ", a, "and" , b, "is", mult)
+
+div = a/b
+print("The division of ", a, "and" , b, "is", div)
+
+exp = a**b
+print("The exponential of ", a, "and" , b, "is", exp)
+
+mod = a%b
+print("The module of ", a, "and" , b, "is", mod)
+
+flotdiv = a//b
+print("The float division of ", a, "and" , b, "is", flotdiv)
+"""
+# fruit = "Mango"
+# mangolen = len(fruit)
+# print(mangolen)
+
+# print(fruit[-1:-3])
+
+# print(fruit[0:5])
+
+# str1 = "Welcome2"
+# print(str1)
+# print(str1.endswith("!!!"))
+
+# print(str1.endswith("lc", 2,4))
+
+# print(str1.find("lc"))
+
+# print(str1.isalnum())
+# print(str1.isalpha())
+
+# a = int(input("Enter a number"))
+# b = 30
+# if(a>b):
+#     print("You can go")
+# else:
+#     print("Stop here")
+
+# import time
+
+# timestamp = time.strftime('%H:%M:%S')
+# print(timestamp)
+# timestamp1 = int(time.strftime('%H'))
+# print(timestamp1)
+# timestamp = time.strftime('%M')
+# print(timestamp)
+# timestamp = time.strftime('%S')
+# print(timestamp)
+
+# if(timestamp1<12):
+#     print("Good Morning")
+# elif(12 <= timestamp1 < 18):
+#     print("Good Afternoon")
+# elif(18 <= timestamp1 < 22):
+#     print("Good Evening")
+# else:
+#     print("Good Night")
+
+# x = int(input("Enter the value of x"))
+
+# match x:
+#     case 0:
+#         print("x is zero")
+#     case 4:
+#         print("x is 4")
+#     case _:
+#         print(x)
+
+name = "Fahad"
+for i in name:
+    print(i, end=(","))
+    if(i == "h"):
+        print("H")
+
+        
